a9dc081e09e8a8e54493b1f78ddcb95f1934b301
romerohenrique <romerohenrique08@gmail.com>
2020-10-16 12:39:03 -0300

    regras da associação

--- br/com/cursojava/javacore/associacaodeclasses7/test/SeminarioTest.java
+++ br/com/cursojava/javacore/associacaodeclasses7/test/SeminarioTest.java
@@ -5,6 +5,16 @@ import br.com.cursojava.javacore.associacaodeclasses7.classe.Local;
 import br.com.cursojava.javacore.associacaodeclasses7.classe.Professor;
 import br.com.cursojava.javacore.associacaodeclasses7.classe.Seminario;
 
+/**
+ * Associação de Classes: Sistema de gerenciamento de seminários
+ * Seminario; Local; Professor; Aluno;
+ * um aluno só poderá estar em um seminário;
+ * um seminario poderá ter nenhum ou vários alunos;
+ * um professor poderá ministrar um ou vários seminários;
+ * um seminário só poderá ter um professor;
+ * um seminário só poderá ter um local
+ */
+
 public class SeminarioTest {
     public static void main(String[] args) {
         Aluno aluno1 = new Aluno("Romero", 33);
