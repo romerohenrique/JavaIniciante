7105b8318020c7c2a8b775b799f404acca55b7a3
romerohenrique <romerohenrique08@gmail.com>
2020-10-30 10:30:53 -0300

    Assert

---
+++ br/com/cursojava/javacore/assertiva/AcertivaTest.java
@@ -0,0 +1,11 @@
+package br.com.cursojava.javacore.assertiva;
+
+public class AcertivaTest {
+    public static void main(String[] args) {
+        calacularSalario(0);
+    }
+
+    private static void calacularSalario(double salario) {
+        assert salario > 0 : "Salario ter que ser maior que zero, salário informado: " + salario;
+    }
+}
--- br/com/cursojava/javacore/exception15/checkedexception/classe/Funcionario.java
+++ br/com/cursojava/javacore/exception15/checkedexception/classe/Funcionario.java
@@ -11,7 +11,6 @@ import java.io.FileNotFoundException;
  */
 
 public class Funcionario extends Pessoa {
-
     @Override
     public void salvar() {
     }
