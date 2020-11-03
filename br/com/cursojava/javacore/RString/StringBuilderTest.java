c69ce72340a59dae481e902cc928318a06b055a3
romerohenrique <romerohenrique08@gmail.com>
2020-11-03 11:07:39 -0300

    StringBuilder .

---
+++ br/com/cursojava/javacore/RString/StringBuilderTest.java
@@ -0,0 +1,22 @@
+package br.com.cursojava.javacore.RString;
+
+/**
+ * A String Builder é uma classe por isso sua inicialização é mais rápida
+ * quando se tratando de várias Strings em um sistemas senod inicializada.
+ */
+
+public class StringBuilderTest {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(19);
+
+        //Forma de ler uma Strig  e/ ou concatenação é chamar oo método "append" .
+        sb.append("pou");
+        System.out.println(sb);
+        //inserindo uma string pegando a posição e passando a nova string
+        sb.insert(3, 3456);
+        System.out.println(sb);
+        //deletando uma string do passando o pponto de inicio e fim
+        sb.delete(3,6);
+        System.out.println(sb);
+    }
+}
