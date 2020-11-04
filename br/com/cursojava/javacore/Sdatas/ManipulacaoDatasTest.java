6efb728a3b8f037e75163a67f7d5616adb95bf32
romerohenrique <romerohenrique08@gmail.com>
2020-11-03 22:46:43 -0300

    Datas atualiação

--- br/com/cursojava/javacore/Sdatas/ManipulacaoDatasTest.java
+++ br/com/cursojava/javacore/Sdatas/ManipulacaoDatasTest.java
@@ -19,6 +19,14 @@ public class ManipulacaoDatasTest {
         System.out.println(c.get(Calendar.DAY_OF_MONTH));
         System.out.println(c.get(Calendar.DAY_OF_WEEK));
         System.out.println(c.get(Calendar.DAY_OF_YEAR));
+        // adicionando horas
+        c.add(Calendar.HOUR, 5);
+        // adiciona  apenas o dia do mês mas não os demais dados da data
+        c.roll(Calendar.DAY_OF_MONTH, 3);
+        Date date2 = c.getTime();
 
+
+        System.out.println(date2);
     }
+
 }
