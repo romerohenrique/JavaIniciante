622a10b95d283585fe8db157723576cc30f62094
romerohenrique <romerohenrique08@gmail.com>
2020-11-04 22:24:45 -0300

    Number Format: formatação de núemros

---
+++ br/com/cursojava/javacore/Sdatas/NumberFormatTest.java
@@ -0,0 +1,46 @@
+package br.com.cursojava.javacore.Sdatas;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+/**
+ * Number Format: formatação de núemros
+ */
+
+public class NumberFormatTest {
+    public static void main(String[] args) throws ParseException {
+
+        float numero = 2334.567f;
+
+        Locale locChina = new Locale("Zh", "ZH");
+        Locale locItaly = new Locale("it", "it");
+        Locale locUsa = new Locale("en", "us");
+        NumberFormat[] nf = new NumberFormat[4];
+        nf[0] = NumberFormat.getIntegerInstance();
+        nf[1] = NumberFormat.getIntegerInstance(locUsa);
+        nf[2] = NumberFormat.getCurrencyInstance();
+        nf[3] = NumberFormat.getCurrencyInstance(locUsa);
+
+        for (NumberFormat nfa : nf) {
+            System.out.println(nfa.format(numero));
+        }
+
+        NumberFormat nf2 = NumberFormat.getInstance();
+        //pega todas as frações
+        System.out.println(nf2.getMaximumFractionDigits());
+
+        //pega o numero máximo da fração
+        nf2.setMaximumFractionDigits(1);
+        System.out.println(nf2.format(numero));
+
+        String valor = "145,6745";
+
+        System.out.println(nf2.parse(valor));
+
+        // passando para inteiro
+        nf2.setParseIntegerOnly(true);
+        System.out.println(nf2.parse(valor));
+
+    }
+}
