624abb82ec163900ad9b0383f8b3847c59b03daf
romerohenrique <romerohenrique08@gmail.com>
2020-11-11 00:44:20 -0300

    Expressões regulares: codificadores

--- br/com/cursojava/javacore/Texpressoesregulares/ExpressoesRegularesTest.java
+++ br/com/cursojava/javacore/Texpressoesregulares/ExpressoesRegularesTest.java
@@ -12,23 +12,29 @@ import java.util.regex.Pattern;
  * \\S = caractere que não é branco;
  * \\w = caractere de palavras a-z A-Z , digitos e _ (underline);
  * \\W = tudo o que não for caracter de palavra;
- *  [] = bloco para busca;
- *  codificadores:
- *  ? zero ou uma;
- *  * zero ou mais;
- *  + uma ou mais;
- *  {n,m} de a n até m;
- *  () condiçoes;
- *  | um ou outro;
- *  $ fim de linha
+ * [] = bloco para busca;
+ * codificadores:
+ * ? zero ou uma;
+ * * zero ou mais;
+ * + uma ou mais;
+ * {n,m} de a n até m;
+ * () condiçoes;
+ * | um ou outro;
+ * $ fim de linha;
+ * . coringa ex: 1.3 == 123 133 1#3 (pode ser substituído).
  */
 
 public class ExpressoesRegularesTest {
     public static void main(String[] args) {
-        // encontrar hexadecimal
-        String regex = "0[xX]([0-9a-fA-F])+(\\s|$)";
-        String texto = "12 0x 0X 0x01FFABC 0x10G 0x1";
-
+//        // encontrar hexadecimal
+//        String regex = "0[xX]([0-9a-fA-F])+(\\s|$)";
+//        String texto = "12 0x 0X 0x01FFABC 0x10G 0x1";
+//        //enconttrar e-mails válidos
+//        String regex = "([a-zA-Z0-9\\._-])+@([a-z\\w])+(\\.+([a-z0-9]+))+";
+//        String texto = "bola12_br@gmail.com pereira@hotmail.gov.br 3m@grupuforte.co.com.br";
+        //encontrar datas
+        String regex = "\\d{1,2}/\\d{1,2}/\\d{2,4}";
+        String texto = "12/12/2012 2/3/20 1/12/2020";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto);
 
@@ -37,7 +43,7 @@ public class ExpressoesRegularesTest {
         System.out.println("Expressão: " + matcher.pattern());
         System.out.println("posições encontradas");
         while (matcher.find()) {
-            System.out.println(matcher.start() + " "+matcher.group());
+            System.out.println(matcher.start() + " " + matcher.group());
         }
     }
 }
