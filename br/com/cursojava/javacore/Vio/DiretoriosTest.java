f46854759b3b6219936586a6ef3a17b0f6e09b3a
romerohenrique <romerohenrique08@gmail.com>
2020-11-12 23:45:21 -0300

    IO:  Criando diretório e renomeando arquivos

---
+++ br/com/cursojava/javacore/Vio/DiretoriosTest.java
@@ -0,0 +1,24 @@
+package br.com.cursojava.javacore.Vio;
+
+import java.io.File;
+import java.io.IOException;
+
+public class DiretoriosTest {
+    public static void main(String[] args) throws IOException {
+        File diretorio = new File("Folder");
+        boolean mkdir = diretorio.mkdir();
+        System.out.println("Diretório criado: " + mkdir);
+        File file = new File(diretorio, "Arquivo.txt");
+        boolean arq = file.createNewFile();
+        System.out.println("Arquivo criado: " + arq);
+
+        // Renomeando aqruivo em um diretório específico
+        File arquivoRenomeado = new File(diretorio, "ArquivoRenomeado.txt");
+        boolean renomeado = file.renameTo(arquivoRenomeado);
+        System.out.println("Arquivo renomeado: " + renomeado);
+        // renomeando um Diretório
+        File diretorioRenomeado = new File("Folder2");
+        boolean dRenomeado = diretorio.renameTo(diretorioRenomeado);
+        System.out.println("Diretório Renomeado: " + dRenomeado);
+    }
+}
