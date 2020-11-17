7db756a7c642fe27664cb135a5b87664cb7f1bee
romerohenrique <romerohenrique08@gmail.com>
2020-11-17 19:53:27 -0300

    NIO: DirectoryStream --> ler arquivos dentro dos diretórios

---
+++ br/com/cursojava/javacore/Wnio/DirectoryStreamTest.java
@@ -0,0 +1,22 @@
+package br.com.cursojava.javacore.Wnio;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DirectoryStreamTest {
+    public static void main(String[] args) {
+        Path dir = Paths.get("pasta");
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)){
+            for(Path path: stream){
+                System.out.println(path.getFileName());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
