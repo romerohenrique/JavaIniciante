b9a9beb73c39a1a6ed9e4f48dfda04acfdc9be6b
romerohenrique <romerohenrique08@gmail.com>
2020-11-20 14:46:58 -0300

    Collections : criando uma lista e aadicionando objetos

---
+++ br/com/cursojava/javacore/Ycolecoes/test/ListTest.java
@@ -0,0 +1,26 @@
+package br.com.cursojava.javacore.Ycolecoes.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest {
+    public static void main(String[] args) {
+        List<String> nome = new ArrayList<>();
+        List<String> nome2 = new ArrayList<>();
+        nome.add("joaqui");
+        nome.add("Pedro");
+        nome2.add("Clemente");
+        nome2.add("Marly");
+        nome.addAll(nome2);
+
+//        for (Object name:nome){
+//            System.out.println(name);
+//        }
+            int size = nome.size();
+
+        //outra forma de percorrer o array list
+        for (int i = 0 ; i <size ; i++) {
+            System.out.println(nome);
+        }
+    }
+}
