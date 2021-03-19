a70a27862f0fec60c974111c0c1f16787971d680
romerohenrique <romerohenrique08@gmail.com>
2021-03-19 16:08:17 -0300

    Introdução à Generics

---
+++ br/com/cursojava/javacore/ZZAgeneric/GenericTest.java
@@ -0,0 +1,32 @@
+package br.com.cursojava.javacore.ZZAgeneric;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Generics é algo que exiiste somente em tempo de
+ * Type erasure
+ * a partir da versão 5, o Java tem retrocompatibilidade com versões anteriores
+ */
+public class GenericTest {
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+        lista.add("maçã");
+        lista.add("Pera");
+        lista.add("ovo");
+
+        for (String obj : lista) {
+            System.out.println(obj);
+        }
+        add(lista, 1L);
+        //em tempo de compilação lançará um erro.
+        for (String obj : lista) {
+            System.out.println(obj);
+        }
+
+
+    }
+    public  static void add (List lista, Long l){
+        lista.add(2);
+    }
+}
