43be8201715c1ad08e16b352401c7f89047101ad
romerohenrique <romerohenrique08@gmail.com>
2021-03-26 11:47:29 -0300

    classes internas estáticas

---
+++ br/com/cursojava/javacore/ZZBclassesinternas/test/ClasseInternasEstaticaTest.java
@@ -0,0 +1,28 @@
+package br.com.cursojava.javacore.ZZBclassesinternas.test;
+
+/**
+ * Classes internas estáticas
+ */
+class Externa {
+    static class Interna {
+        public void imprimir() {
+            System.out.println("Classe estática");
+        }
+    }
+}
+
+public class ClasseInternasEstaticaTest {
+    public static void main(String[] args) {
+
+        Externa.Interna i1 = new Externa.Interna();
+        i1.imprimir();
+        Interna2 i2 = new Interna2();
+        i2.imprimir();
+    }
+
+    static class Interna2 {
+        public void imprimir() {
+            System.out.println("Classe estática, dentro da classe publica");
+        }
+    }
+}
