62f51507a46d938b5a25f3d9d009cd1c892e4daa
NAR <romerohenrique08@gmail.com>
2021-04-07 16:48:38 -0300

    Classe Carro para o CRUD

---
+++ br/com/cursojava/javacore/ZZCjdbc/classe/Carro.java
@@ -0,0 +1,67 @@
+package br.com.cursojava.javacore.ZZCjdbc.classe;
+
+import java.util.Objects;
+
+public class Carro {
+    private Integer id;
+    private String nome;
+    private String placa;
+    private Comprador comprador;
+
+    public Carro(Integer id, String nome, String placa, Comprador comprador) {
+        this.id = id;
+        this.nome = nome;
+        this.placa = placa;
+        this.comprador = comprador;
+    }
+
+
+    public Carro() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Carro carro = (Carro) o;
+        return Objects.equals(id, carro.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+
+    public Comprador getComprador() {
+        return comprador;
+    }
+
+    public void setComprador(Comprador comprador) {
+        this.comprador = comprador;
+    }
+}
