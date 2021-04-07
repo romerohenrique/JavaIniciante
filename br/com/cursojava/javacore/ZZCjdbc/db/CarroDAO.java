7fc0bcd96a7ddd99ed4f0054627c5130f9efabac
NAR <romerohenrique08@gmail.com>
2021-04-07 16:48:22 -0300

    Classe DAO

---
+++ br/com/cursojava/javacore/ZZCjdbc/db/CarroDAO.java
@@ -0,0 +1,101 @@
+package br.com.cursojava.javacore.ZZCjdbc.db;
+
+import br.com.cursojava.javacore.ZZCjdbc.classe.Carro;
+import br.com.cursojava.javacore.ZZCjdbc.classe.Comprador;
+import br.com.cursojava.javacore.ZZCjdbc.com.ConexaoFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarroDAO {
+    public static void save(Carro carro) {
+        String sql = "INSERT INTO `agencia`.`carro` (`placa`,`nome`,  comprador_id) VALUES (? , ?, ?)";
+
+        try (Connection conn = ConexaoFactory.getConexao();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, carro.getPlaca());
+            ps.setString(2, carro.getNome());
+            ps.setInt(3, carro.getComprador().getId());
+            ps.executeUpdate();
+            System.out.println("Registro criado com sucesso");
+        } catch (SQLException e) {
+            ;
+            e.printStackTrace();
+        }
+    }
+
+    public static void delete(Carro carro) {
+        if (carro == null || carro.getId() == null) {
+            System.out.println("Não há registros no banco de dados");
+            return;
+        }
+        String sql = "DELETE FROM `agencia`.`carro` WHERE `id` = ?;";
+
+        try (Connection conn = ConexaoFactory.getConexao();
+             PreparedStatement ps = conn.prepareStatement(sql);) {
+            ps.setInt(1, carro.getId());
+            ps.executeUpdate();
+            System.out.println("Registro deletado com sucesso");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void update(Carro carro) {
+        if (carro == null || carro.getId() == null) {
+            System.out.println("Não há registros no banco de dados");
+            return;
+        }
+        String sql = "UPDATE `agencia`.`carro` SET `placa` = ?, `nome` = ? WHERE `id` = ?";
+        try (Connection conn = ConexaoFactory.getConexao();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, carro.getPlaca());
+            ps.setString(2, carro.getNome());
+            ps.setInt(3, carro.getId());
+            ps.executeUpdate();
+            System.out.println("Registro atualizado com sucesso");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Carro> selectAll() {
+        String sql = "SELECT id, nome, placa, comprador_id  FROM agencia.carro;";
+        List<Carro> carroList = new ArrayList<>();
+        try (Connection conn = ConexaoFactory.getConexao();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery();) {
+            while (rs.next()) {
+                Comprador c = CompradorDAO.searchById(rs.getInt("comprador_id"));
+                carroList.add(new Carro(rs.getInt("id"), rs.getString("nome"), rs.getString("placa"), c));
+            }
+            return carroList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static List<Carro> searchByName(String nome) {
+        String sql = "SELECT id,  nome, placa, comprador_id FROM agencia.carro where nome LIKE ?";
+        List<Carro> carroList = new ArrayList<>();
+        try (Connection conn = ConexaoFactory.getConexao();
+             PreparedStatement ps = conn.prepareStatement(sql);) {
+            ps.setString(1, "%" + nome + "%");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Comprador c = CompradorDAO.searchById(rs.getInt("comprador_id"));
+                carroList.add(new Carro(rs.getInt("id"), rs.getString("cpf"), rs.getString("nome"), c));
+            }
+            ConexaoFactory.close(conn, ps, rs);
+            return carroList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
